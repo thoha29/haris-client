894bff684ae99274843892e450ac446a5eea7cc2
farandeffraim <93232915+farandeffraim@users.noreply.github.com>
2026-06-03 17:03:32 +0700

    Update HrdApproval.jsx
    hapus approval hrd

--- src/pages/menu-hrd/Absensi/HrdApproval.jsx
+++ src/pages/menu-hrd/Absensi/HrdApproval.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
 // import axios from 'axios';
-import Swal from 'sweetalert2';
 import './HrdApproval.css';
 import api from '../../../config/api';
 
@@ -13,7 +12,7 @@ const HrdApproval = () => {
     setLoading(true);
     setError(null);
     try {
-      const res = await api.get('/absensi/hrd/pending-hrd');
+     const res = await api.get('/absensi');
       setListPending(res.data);
     } catch (err) {
       console.error('Error Detail:', err.response);
@@ -28,40 +27,14 @@ const HrdApproval = () => {
     fetchPending();
   }, [fetchPending]);
 
-  const handleAction = async (id, status) => {
-    const result = await Swal.fire({
-      title: 'Konfirmasi',
-      text: `Yakin ingin ${status} absensi ini?`,
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Ya',
-      cancelButtonText: 'Batal',
-    });
 
-    if (!result.isConfirmed) return;
 
-    try {
-      await api.put('/absensi/hrd/approve-hrd', {
-        id_data_absensi: id,
-        status: status,
-      });
-      Swal.fire('Berhasil!', `Absensi berhasil di-${status}`, 'success');
-      fetchPending();
-    } catch (err) {
-      Swal.fire(
-        'Error',
-        'Error: ' + (err.response?.data?.error || 'Gagal proses.'),
-        'error'
-      );
-    }
-  };
+
 
   return (
     <div className="hrd-approval-container">
       <div className="header">
-        <h2>Panel Approval & Monitoring Karyawan</h2>
+       <h2>Monitoring Absensi Karyawan</h2>
         <button
           onClick={fetchPending}
           className="btn-refresh"
@@ -80,16 +53,15 @@ const HrdApproval = () => {
       {!loading && !error && (
         <div className="table-wrapper">
           <table className="approval-table">
-            <thead>
-              <tr>
-                <th>Karyawan</th>
-                <th>Tanggal</th>
-                <th>Masuk</th>
-                <th>Keluar</th>
-                <th>Status Atasan</th>
-                <th>Tindakan</th>
-              </tr>
-            </thead>
+          <thead>
+            <tr>
+              <th>Karyawan</th>
+              <th>Tanggal</th>
+              <th>Masuk</th>
+              <th>Keluar</th>
+              <th>Status Approval</th>
+            </tr>
+          </thead>
             <tbody>
               {listPending.length > 0 ? (
                 listPending.map((item) => (
@@ -104,38 +76,19 @@ const HrdApproval = () => {
                     <td>{item.jam_keluar || '--:--'}</td>
 
                     <td className="text-center">
-                      <span className={`badge-status ${item.status_user}`}>
-                        {item.status_user
-                          ? item.status_user.toUpperCase()
-                          : 'N/A'}
-                      </span>
-                    </td>
+                    <span className={`badge-status ${item.is_approved}`}>
+                      {item.is_approved
+                        ? item.is_approved.toUpperCase()
+                        : 'N/A'}
+                    </span>
+                  </td>
 
-                    <td>
-                      <div className="action-group">
-                        <button
-                          onClick={() =>
-                            handleAction(item.id_data_absensi, 'approved')
-                          }
-                          className="btn btn-approve"
-                        >
-                          Approve
-                        </button>
-                        <button
-                          onClick={() =>
-                            handleAction(item.id_data_absensi, 'rejected')
-                          }
-                          className="btn btn-reject"
-                        >
-                          Reject
-                        </button>
-                      </div>
-                    </td>
+     
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan="6" className="empty-state">
+                  <td colSpan="5" className="empty-state">
                     🚫 Tidak ada pengajuan pending.
                   </td>
                 </tr>
